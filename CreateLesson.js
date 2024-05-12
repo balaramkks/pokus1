77a9a062a79331618752b54d889657ed126d7505
balaramkks <167354496+balaramkks@users.noreply.github.com>
2024-05-12 22:10:56 +0200

    Add files via upload

--- CreateLesson.js
+++ CreateLesson.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import { fetchLessons } from '../App';
 
 function CreateLesson(props) {
   const [showCreateModal, setShowCreateModal] = useState(false);
